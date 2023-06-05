9dbe42c0d1147da4123773ea546d99735545c7a5
Sheikh Nafis <sk.nafis33@yahoo.com>
2023-06-05 14:42:43 +0600

    8th commit

--- home/models.py
+++ home/models.py
@@ -1,53 +1,62 @@
 from django.db import models
 
 class Game(models.Model):
+    id = models.AutoField(primary_key=True)
     g_img = models.ImageField(upload_to='./games/')
     g_title = models.CharField(max_length=100)
     g_description = models.CharField(max_length=500)
 
 class Desktop(models.Model):
+    id = models.AutoField(primary_key=True)
     img = models.ImageField(upload_to='./pc/')
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=500)
     price = models.IntegerField()
 
 class Laptop(models.Model):
+    id = models.AutoField(primary_key=True)
     l_img = models.ImageField(upload_to='./lp/')
     l_title = models.CharField(max_length=100)
     l_description = models.CharField(max_length=500)
     l_price = models.IntegerField()
 
 class Monitor(models.Model):
+    id = models.AutoField(primary_key=True)
     img = models.ImageField(upload_to='./m/')
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=500)
     price = models.IntegerField()
 
 class Component(models.Model):
+    id = models.AutoField(primary_key=True)
     img = models.ImageField(upload_to='./c/')
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=500)
     price = models.IntegerField()
 
 class Accessorie(models.Model):
+    id = models.AutoField(primary_key=True)
     img = models.ImageField(upload_to='./a/')
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=500)
     price = models.IntegerField()
 
 class Gadget(models.Model):
+    id = models.AutoField(primary_key=True)
     img = models.ImageField(upload_to='./g/')
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=500)
     price = models.IntegerField()
 
 class Tv(models.Model):
+    id = models.AutoField(primary_key=True)
     img = models.ImageField(upload_to='./t/')
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=500)
     price = models.IntegerField()
 
 class Phone(models.Model):
+    id = models.AutoField(primary_key=True)
     img = models.ImageField(upload_to='./p/')
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=500)
